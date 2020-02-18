5caa5fcfcf7b3953238ceeaefbde0cf4d4d7d690
Rook <rook@rook.merand.org>
2020-02-17 21:51:43 -0500

    remove dependency on bc

--- bin/rotate_backups.sh
+++ bin/rotate_backups.sh
@@ -15,17 +15,17 @@ cd backup
 for DOC in $(ls | awk 'BEGIN{FS="."}{print $1}' | uniq); do
   SEARCH="${DOC}*.html" 
   COUNT=$(ls ${SEARCH} | wc -l)
-  OMIT=$(bc -e "${COUNT} - ${NUM_BACKUPS}" -e 'quit')
+  EXTRA=$(awk "BEGIN{printf ${COUNT}-${NUM_BACKUPS}}")
   # Only zip things up if the number of backups is high enough
-  if [ "${OMIT}" -gt 0 ]; then
+  if [ "${EXTRA}" -gt 0 ]; then
     TAR="${DOC}.$(date +'%s').tar"
     # create a tar from a single file (otherwise the append will barf)
     find . -iname "${SEARCH}" -print | head -n 1 | xargs tar -cvf "${TAR}"
     # append to the tar
-    find . -iname "${SEARCH}" -print | head -n "${OMIT}" | xargs tar -rvf "${TAR}"
+    find . -iname "${SEARCH}" -print | head -n "${EXTRA}" | xargs tar -rvf "${TAR}"
     # gzip the tar
     gzip "${TAR}"
     # remove the files
-    find . -iname "${SEARCH}" -print | head -n "${OMIT}" | xargs rm
+    find . -iname "${SEARCH}" -print | head -n "${EXTRA}" | xargs rm
   fi
 done
